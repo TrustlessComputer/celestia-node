5c063aa15acd998217c573d061869f017e475cef
69tc <696969tc@gmail.com>
2024-01-21 12:44:39 +0700

    update

--- cmd/da-server/apis/apiNearDa.go
+++ cmd/da-server/apis/apiNearDa.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	DA_KEY      = "ed25519:4U1jUuRG6xkRBv1pMyiwFV3jeSP3qQVZnp2FWzFD3o46pPU4SE3xtBRbPPZvrV7W22mbodCV6NhpSGxwYr5mhGhe"
-	DA_CONTRACT = "nearda2.testnet"
-	DA_ACCOUNT  = "phuong"
+	DA_CONTRACT = "nearda3.testnet"
+	DA_ACCOUNT  = "nearda3.testnet"
 )
 
 func ApiTestNearDA(w http.ResponseWriter, r *http.Request) {
