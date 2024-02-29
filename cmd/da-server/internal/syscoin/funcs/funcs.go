5703bdbb7e58d908468df13d762c1ee3511b5a5a
axe31803 <axe31803@generative.xyz>
2024-02-29 14:17:28 +0700

    Impl - syscoin - 2

--- cmd/da-server/internal/syscoin/funcs/funcs.go
+++ cmd/da-server/internal/syscoin/funcs/funcs.go
@@ -26,18 +26,20 @@ func createReqHeader(req *http.Request) {
 	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", credential()))
 }
 
-func sysRequest(syscoinMethod string, params string) ([]byte, error) {
+func sysRequest(syscoinMethod string, params []interface{}) ([]byte, error) {
 	cnf := config.GetConfig()
 	client := &http.Client{}
 
-	if params == "" {
-		params = "[]"
-	} else {
-		params = fmt.Sprintf(`["%s"]`, params)
+	paramsStr := "[]"
+	if len(params) != 0 {
+		_b, err1 := json.Marshal(params)
+		if err1 != nil {
+			return nil, err1
+		}
+		paramsStr = string(_b)
 	}
-
 	id := fmt.Sprintf("bmv-%d", time.Now().UTC().UnixNano())
-	requestData := fmt.Sprintf(`{"jsonrpc": "1.0", "id": "%s", "method": "%s", "params": %s}`, id, syscoinMethod, params)
+	requestData := fmt.Sprintf(`{"jsonrpc": "1.0", "id": "%s", "method": "%s", "params": %s}`, id, syscoinMethod, paramsStr)
 	req, err := http.NewRequest(
 		"POST",
 		cnf.RpcURL,
@@ -53,53 +55,73 @@ func sysRequest(syscoinMethod string, params string) ([]byte, error) {
 		return nil, errors.WithStack(err)
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, errors.WithStack(err)
-		}
-
-		d := &SysRespErr{}
-		err = json.Unmarshal(body, d)
-		if err != nil {
-			return nil, errors.WithStack(err)
-		}
-
-		return nil, errors.WithStack(errors.New(fmt.Sprintf("%d, %s", d.Error.Code, d.Error.Message)))
-	}
-
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
-	return body, nil
+	d := &SyscoinRPCResp{}
+	err = json.Unmarshal(body, d)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if d.Error != nil {
+		return nil, errors.WithStack(errors.New(fmt.Sprintf("%d, %s", d.Error.Code, d.Error.Message)))
+	}
+
+	_b, err := json.Marshal(d.Result)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return _b, nil
 }
 
 func UploadData(data []byte) (string, error) {
 	var err error
 	dataBlobInHex := hex.EncodeToString(data)
-	body, err := sysRequest(METHOD_CREATE_EVM_BLOD, dataBlobInHex)
+	body, err := sysRequest(METHOD_CREATE_EVM_BLOD, []interface{}{dataBlobInHex})
 	if err != nil {
 		return "", errors.WithStack(err)
 	}
 
-	respData := SyscoinRPCResp{}
+	respData := SyscoinUploadResp{}
 	err = json.Unmarshal(body, &respData)
 	if err != nil {
 		return "", errors.WithStack(err)
 	}
-	return respData.result, nil
+
+	return respData.Versionhash, nil
 }
 
 func GetData(hash string) ([]byte, error) {
-	//TODO - implement me
-	return nil, nil
+	var err error
+
+	//https://docs.syscoin.org/docs/tech/poda#getnevmblobdata
+	//example "params": ["hash_string", true]
+	body, err := sysRequest(METHOD_GET_EVM_BLOD, []interface{}{hash, true})
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	respData := SyscoinGetUploadedResp{}
+	err = json.Unmarshal(body, &respData)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_b, err := hex.DecodeString(respData.Data)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return _b, nil
 }
 
 // test
 func Getblockchaininfo() ([]byte, error) {
-	resp, err := sysRequest(METHOD_GET_BLOCK_INFO, "")
+	resp, err := sysRequest(METHOD_GET_BLOCK_INFO, []interface{}{})
 	if err != nil {
 		return nil, err
 	}
--- cmd/da-server/internal/syscoin/funcs/struct.go
+++ cmd/da-server/internal/syscoin/funcs/struct.go
@@ -2,19 +2,28 @@ package funcs
 
 const (
 	METHOD_CREATE_EVM_BLOD string = "syscoincreatenevmblob"
+	METHOD_GET_EVM_BLOD    string = "getnevmblobdata"
 	METHOD_GET_BLOCK_INFO  string = "getblockchaininfo"
 )
 
 type SyscoinRPCResp struct {
-	jsonrpc string
-	result  string
+	Result interface{}     `json:"result"`
+	Error  *SyscoinErrResp `json:"error"`
+	Id     string          `json:"id"`
 }
 
-type SysRespErr struct {
-	Result interface{} `json:"result"`
-	Error  struct {
-		Code    int    `json:"code"`
-		Message string `json:"message"`
-	} `json:"error"`
-	Id string `json:"id"`
+type SyscoinUploadResp struct {
+	Versionhash string `json:"versionhash"`
+}
+
+type SyscoinGetUploadedResp struct {
+	Versionhash string `json:"versionhash"`
+	Mpt         int    `json:"mpt"`
+	Datasize    int    `json:"datasize"`
+	Data        string `json:"data"`
+}
+
+type SyscoinErrResp struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
 }
